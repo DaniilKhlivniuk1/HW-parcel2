c73b15cafb030a78940627bccf8da674f9b6d286
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2025-03-16 12:42:12 +0200

    q

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-import { addTask, displayTasks } from './modules/task.js';
+// import { addTask, displayTasks } from './modules/task.js';
 
-const form = document.getElementById('task-form');
-const taskInput = document.getElementById('task-input');
-const taskList = document.getElementById('task-list');
-
-
-document.addEventListener('DOMContentLoaded', displayTasks);
-
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const task = taskInput.value;
-  addTask(task);
-  taskInput.value = ''; 
-  displayTasks();
-});
+import "./app.js"
+import "./task.js"
